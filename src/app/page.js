4d633c1cd3bf0cb3c4259adc8b7df54633f7aa51
Eddy Chen <eddychen615@gmail.com>
2023-11-05 13:25:30 +0900

    try

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,38 @@
-import Image from 'next/image';
+// Import Swiper React components
+'use client';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default function Home() {
-  return <div>123</div>;
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import './globals.css';
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
+export default function App() {
+  return (
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>
+          <img src="/img/30Ywoman-04-bg.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/happypeople.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/office-bg.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
