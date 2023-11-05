e1b0e5285338ff42c5af0ba88098df8595dc724a
Eddy Chen <eddychen615@gmail.com>
2023-11-05 14:54:56 +0900

    loop

---
+++ src/app/Slider.js
@@ -0,0 +1,46 @@
+// Import Swiper React components
+'use client';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import './globals.css';
+
+export default function Slider() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        loop={true}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="/img/office-bg.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/30Ywoman-04-bg.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/img/happypeople.png" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,11 @@
-// Import Swiper React components
-'use client';
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-
-import './globals.css';
-
-// import required modules
-import { Navigation } from 'swiper/modules';
+import Swiper from './Slider';
+import Header from './Header';
 
 export default function App() {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>
-          <img src="/img/30Ywoman-04-bg.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/img/happypeople.png" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/img/office-bg.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+      <Header />
+      <Swiper />
     </>
   );
 }
