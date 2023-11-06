438ad49798b44a51550b694cb8d3a3e6fed3136a
Eddy Chen <eddychen615@gmail.com>
2023-11-06 11:12:21 +0900

    hover

--- src/app/Header.js
+++ src/app/Header.js
@@ -5,7 +5,9 @@ import './css/Header.css';
 export default function Header() {
   return (
     <div className="header fixed top-0 left-0 z-10 p-8">
-      <img src="/img/ourly_logo_w600.png" className="w-56" />
+      <a href="https://service.ourly.jp/">
+        <img src="/img/ourly_logo_w600.png" className="w-56" />
+      </a>
       <button className="otoiawase ml-auto text-[18px]">お問い合わせ</button>
     </div>
   );
