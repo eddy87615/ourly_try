47c61572781939f22d51bb873d50c75f17ede6d0
Eddy Chen <eddychen615@gmail.com>
2023-11-07 10:24:12 +0900

    intro css

--- src/app/Intro.js
+++ src/app/Intro.js
@@ -3,5 +3,11 @@ import './css/globals.css';
 import './css/Intro.css';
 
 export default function Intro() {
-  return <div className="intro">234</div>;
+  return (
+    <div className="intro">
+      <p className="text-[40px] font-bold intro-text">
+        あなたも会社のこと悩んでいますか？
+      </p>
+    </div>
+  );
 }
