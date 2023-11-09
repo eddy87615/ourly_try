34d20a2b2b7ed6076d133c66d979e88c65caf955
Eddy Chen <eddychen615@gmail.com>
2023-11-09 12:11:09 +0900

    components

--- src/app/Main.js
+++ src/app/Main.js
@@ -2,17 +2,22 @@ import React from 'react';
 import './css/globals.css';
 import './css/Main.css';
 
-const Section = () => {
+const Section = ({
+  imgAnnoying,
+  imgSolved,
+  annoyingImgInfo,
+  solvedImgInfo,
+}) => {
   return (
     <main className="main">
       <div className="annoying">
         <h2 className="text-[40px] font-bold">
           私も会社のコミュニケーションについて悩んでいます…
         </h2>
-        <img src="/img/20Yman-02-bgN.png" />
+        <img src={imgAnnoying} alt={annoyingImgInfo} />
       </div>
       <div className="solved">
-        <img src="/img/20Yman-04-bgN.png" />
+        <img src={imgSolved} alt={solvedImgInfo} />
       </div>
     </main>
   );
@@ -21,7 +26,12 @@ const Section = () => {
 export default function Main() {
   return (
     <>
-      <Section />
+      <Section
+        imgAnnoying="/img/20Yman-02-bgN.png"
+        annoyingImgInfo="young man is annoyed"
+        imgSolved="/img/20Yman-04-bgN.png"
+        solvedImgInfo="happy young man cause problems solved"
+      />
       <Section />
       <Section />;
     </>
